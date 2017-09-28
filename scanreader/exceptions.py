51bf76caaf2bdd62f46f458904b9ab201abb7a08
ecobost <emcobost@gmail.com>
2017-09-27 20:53:48 -0500

    minor formatting

--- scanreader/exceptions.py
+++ scanreader/exceptions.py
@@ -3,13 +3,13 @@ class ScanReaderException(Exception):
     pass
 
 class ScanImageVersionError(ScanReaderException):
-    """Exception for unsupported ScanImage versions. """
+    """ Exception for unsupported ScanImage versions."""
     pass
 
 class PathnameError(ScanReaderException):
-    """ Exception for dealing with paths and pathname patterns (wildcards). """
+    """ Exception for dealing with paths and pathname patterns (wildcards)."""
     pass
 
 class FieldDimensionMismatch(ScanReaderException):
-    """Exception for trying to slice an array with fields of different dimensions."""
+    """ Exception for trying to slice an array with fields of different dimensions."""
     pass
